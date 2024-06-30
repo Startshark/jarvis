85a35aaaa2070af30c4984407fb3e30f312771ab
startshark <206740779@qq.com>
2024-06-30 21:01:50 +0800

    Basic packages.

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,20 @@
+import os
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
-import dwspark
 
+# GUI界面
+import gradio as gr
+
+# 加载sdk配置
+from sparkai.core.messages import ChatMessage
+from dwspark.config import Config
+
+# 加载对话模型
+from dwspark.models import ChatModel
+
+# 加载讯飞的api
+SPARKAI_APP_ID = os.environ["SPARKAI_APP_ID"]
+SPARKAI_API_SECRET = os.environ["SPARKAI_API_SECRET"]
+SPARKAI_API_KEY = os.environ["SPARKAI_API_KEY"]
+config = Config(SPARKAI_APP_ID, SPARKAI_API_KEY, SPARKAI_API_SECRET)
